0d7f17a976bac4ec9c04ee400a2354b582550276
Marco Antonio Castro Vazquez <marco.castro.vazquez@gmail.com>
2020-02-15 22:46:43 -0600

    Mitad de Proyecto

--- Lexico/Form1.cs
+++ Lexico/Form1.cs
@@ -24,6 +24,7 @@ namespace Lexico
             bool Identificador = true;
             bool Real = false;
             bool Punto = false;
+            bool Or = false;
             string Texto = TxtIngreseTexto.Text;
             int Estado = 0;
             for (int Indice = 0; Indice < Texto.Length; Indice++)
@@ -46,7 +47,7 @@ namespace Lexico
                                 Fila.Cells[2].Value = "3";
                                 Tabla.Rows.Add(Fila);
                             }
-                            if (CodigoASCCI >= 48 && CodigoASCCI <= 57 && !Real)
+                            if (CodigoASCCI >= 48 && CodigoASCCI <= 57 && !Punto)
                             {
                                 Estado = 0;
                                 Real = true;
@@ -73,6 +74,7 @@ namespace Lexico
                             {
                                 Estado = 0;
                                 Punto = true;
+                                Identificador = false;
                                 DataGridViewRow Fila = new DataGridViewRow();
                                 Fila.CreateCells(Tabla);
                                 Fila.Cells[0].Value = "Punto";
@@ -91,6 +93,134 @@ namespace Lexico
                                 Fila.Cells[1].Value = Texto[Indice];
                                 Fila.Cells[2].Value = "5";
                                 Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 42 || CodigoASCCI == 47)
+                            {
+                                Estado = 0;
+                                Real = false;
+                                Identificador = false;
+                                DataGridViewRow Fila = new DataGridViewRow();
+                                Fila.CreateCells(Tabla);
+                                Fila.Cells[0].Value = "OpMul";
+                                Fila.Cells[1].Value = Texto[Indice];
+                                Fila.Cells[2].Value = "6";
+                                Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 61)
+                            {
+                                Estado = 0;
+                                Real = false;
+                                Identificador = false;
+                                DataGridViewRow Fila = new DataGridViewRow();
+                                Fila.CreateCells(Tabla);
+                                Fila.Cells[0].Value = "Asignacion";
+                                Fila.Cells[1].Value = Texto[Indice];
+                                Fila.Cells[2].Value = "18";
+                                Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 59)
+                            {
+                                Estado = 0;
+                                Real = false;
+                                Identificador = false;
+                                DataGridViewRow Fila = new DataGridViewRow();
+                                Fila.CreateCells(Tabla);
+                                Fila.Cells[0].Value = "PuntoComa";
+                                Fila.Cells[1].Value = Texto[Indice];
+                                Fila.Cells[2].Value = "12";
+                                Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 44)
+                            {
+                                Estado = 0;
+                                Real = false;
+                                Identificador = false;
+                                DataGridViewRow Fila = new DataGridViewRow();
+                                Fila.CreateCells(Tabla);
+                                Fila.Cells[0].Value = "Coma";
+                                Fila.Cells[1].Value = Texto[Indice];
+                                Fila.Cells[2].Value = "13";
+                                Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 40)
+                            {
+                                Estado = 0;
+                                Real = false;
+                                Identificador = false;
+                                DataGridViewRow Fila = new DataGridViewRow();
+                                Fila.CreateCells(Tabla);
+                                Fila.Cells[0].Value = "AbreParentesis";
+                                Fila.Cells[1].Value = Texto[Indice];
+                                Fila.Cells[2].Value = "14";
+                                Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 41)
+                            {
+                                Estado = 0;
+                                Real = false;
+                                Identificador = false;
+                                DataGridViewRow Fila = new DataGridViewRow();
+                                Fila.CreateCells(Tabla);
+                                Fila.Cells[0].Value = "CierraParentesis";
+                                Fila.Cells[1].Value = Texto[Indice];
+                                Fila.Cells[2].Value = "15";
+                                Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 123)
+                            {
+                                Estado = 0;
+                                Real = false;
+                                Identificador = false;
+                                DataGridViewRow Fila = new DataGridViewRow();
+                                Fila.CreateCells(Tabla);
+                                Fila.Cells[0].Value = "AbreLlaves";
+                                Fila.Cells[1].Value = Texto[Indice];
+                                Fila.Cells[2].Value = "16";
+                                Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 125)
+                            {
+                                Estado = 0;
+                                Real = false;
+                                Identificador = false;
+                                DataGridViewRow Fila = new DataGridViewRow();
+                                Fila.CreateCells(Tabla);
+                                Fila.Cells[0].Value = "CierraLlaves";
+                                Fila.Cells[1].Value = Texto[Indice];
+                                Fila.Cells[2].Value = "17";
+                                Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 33)
+                            {
+                                Estado = 0;
+                                Real = false;
+                                Identificador = false;
+                                DataGridViewRow Fila = new DataGridViewRow();
+                                Fila.CreateCells(Tabla);
+                                Fila.Cells[0].Value = "OpNot";
+                                Fila.Cells[1].Value = Texto[Indice];
+                                Fila.Cells[2].Value = "10";
+                                Tabla.Rows.Add(Fila);
+                            }
+                            if (CodigoASCCI == 124)
+                            {
+                                if (Or)
+                                {
+                                    DataGridViewRow Fila = new DataGridViewRow();
+                                    Fila.CreateCells(Tabla);
+                                    Fila.Cells[0].Value = "OpNor";
+                                    Fila.Cells[1].Value = Texto[Indice];
+                                    Fila.Cells[2].Value = "10";
+                                    Tabla.Rows.Add(Fila);
+                                    Or = false;
+                                }
+                                else
+                                {
+                                    Estado = 0;
+                                    Real = false;
+                                    Identificador = false;
+                                    Or = true;
+                                }      
                             }
                             break;
                         }
